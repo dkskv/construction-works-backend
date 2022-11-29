b80640e7c7eb1c4e9395c3a908670733a196c8ac
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-11-29 19:07:23 +0300

    Проверка работы Api на Vercel

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 const app = express();
 const router = express.Router();
 app.use(bodyParser.json());
-app.use(express.static("dist"));
 app.use("/", router);
 
 router.get("/hello", function (_, res) {
@@ -13,6 +12,10 @@ router.get("/hello", function (_, res) {
 
 router.get("/api", function (_, res) {
   res.status(200).send("api!");
+});
+
+router.get("/api/hello", function (_, res) {
+  res.status(200).send("api hello!");
 });
 
 app.listen(4000);
