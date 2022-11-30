de6c15c72bd1cf77109a16f10b2a7710c811c54f
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-11-30 14:52:21 +0300

    feat: добавил поддержку cors-запросов

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { ServiceRequestController } from "../src/ServiceRequestController";
+import cors from "cors";
 // @ts-ignore
 import { config } from "dotenv";
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 app.use(bodyParser.json());
 app.use("/", router);
+app.use(cors());
 
 router.get("/api", function (_, res) {
   res.status(200).send("api!");
