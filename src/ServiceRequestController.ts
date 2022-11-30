c6274b720b709935481906ac04a15901ded80002
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-11-30 15:01:06 +0300

    cors test

--- src/ServiceRequestController.ts
+++ src/ServiceRequestController.ts
@@ -21,17 +21,6 @@ export class ServiceRequestController {
 
   private defineRoute() {
     this.router.post("/api/service-request", (req, res) => {
-      res.setHeader("Access-Control-Allow-Credentials", String(true));
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-      );
-
       if (!req.body) {
         res.status(400).send({ message: "body is missing" });
         return;
